c2889c1fc5a56d71d9fc92cc6406f49de7ae73f7
gh <roark2120@gmail.com>
2025-12-13 16:23:20 +0100

    Configure basePath for GitHub Pages

--- embuild-analyses/next.config.ts
+++ embuild-analyses/next.config.ts
@@ -1,10 +1,15 @@
 import { withContentlayer } from 'next-contentlayer'
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'data-blog';
+
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
   trailingSlash: true,
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
 };
 
 export default withContentlayer(nextConfig);
